80e34a9ec7a074a2d86ff181893723145894b0a6
Fritdz <hafidzzz@hotmail.sg>
2021-06-08 14:23:22 +0800

    Enhancement 1

--- app/src/main/java/sg/edu/rp/c346/id20033454/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20033454/simpletodo/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -17,8 +19,10 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spnChoice;
     EditText etTask;
     Button btnAdd;
+    Button btnDel;
     Button btnClr;
     ListView lvToDo;
     ArrayList<String> alTasks;
@@ -28,10 +32,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spnChoice=findViewById(R.id.spinner);
         etTask=findViewById(R.id.etTask);
         btnAdd=findViewById(R.id.btnAdd);
+        btnDel=findViewById(R.id.btnDel);
         btnClr=findViewById(R.id.btnClr);
         lvToDo=findViewById(R.id.lvToDo);
+
+        btnDel.setEnabled(false);
+        etTask.setHint("Type in a new task here");
 
         alTasks = new ArrayList<String>();
         ArrayAdapter aatodo = new ArrayAdapter(this, android.R.layout.simple_list_item_1, alTasks);
@@ -43,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 String newTask = etTask.getText().toString();
                 alTasks.add(newTask);
                 aatodo.notifyDataSetChanged();
+                etTask.setText("");
             }
         });
 
@@ -51,9 +61,43 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 alTasks.clear();
                 aatodo.notifyDataSetChanged();
+                etTask.setText("");
             }
         });
 
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int delTask = Integer.parseInt((etTask.getText().toString()));
+                alTasks.remove(delTask);
+                aatodo.notifyDataSetChanged();
+                etTask.setText("");
+            }
+        });
+
+        spnChoice.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0: //Add
+                        btnAdd.setEnabled(true);
+                        btnDel.setEnabled(false);
+                        etTask.setHint("Type in a new task here");
+                        break;
+                    case 1: //Delete
+                        btnAdd.setEnabled(false);
+                        btnDel.setEnabled(true);
+                        etTask.setHint("Type in the index of the task to be removed");
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
 
